a3f89126b33d55878aba3416e8e06d0b3a6f2b67
ksushkaHrushka <kseniamirt@gmail.com>
2024-04-16 22:19:57 +0300

    four

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import { useTelegram } from '../../hooks/useTelegram';
 
 const Header = () => {
 
-    const {user, onClose} = useTelegram; 
+    const {user, onClose} = useTelegram(); 
 
     return (
         <div className={'header'}>
